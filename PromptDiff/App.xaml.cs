7b49dc4d8a73d9ceac5232cc49af66187fd3f794
UmsrAkne <sibata486@gmail.com>
2026-01-02 19:21:18 +0900

    Ensure `local_data` folder creation during application startup

--- PromptDiff/App.xaml.cs
+++ PromptDiff/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 using Prism.Ioc;
 using PromptDiff.Views;
 
@@ -9,6 +11,25 @@ namespace PromptDiff;
 /// </summary>
 public partial class App
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        // Ensure a folder named "local_data" exists in the executable directory at startup
+        try
+        {
+            var baseDir = AppContext.BaseDirectory;
+            var localDataPath = Path.Combine(baseDir, "local_data");
+            Directory.CreateDirectory(localDataPath);
+        }
+        catch
+        {
+            // Swallow exceptions to avoid blocking app startup; optionally log here in the future.
+        }
+        finally
+        {
+            base.OnStartup(e);
+        }
+    }
+
     protected override Window CreateShell()
     {
         return Container.Resolve<MainWindow>();
